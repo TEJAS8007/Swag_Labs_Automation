ab789d8c0338e96c750c7cd6dea1bb512f9a5a53
Tejas <awaretejas55@gamil.com>
2025-07-27 20:18:04 +0530

    Second commit with updated utilities

--- src/test/java/Com/QA/Utilities/Utilities.java
+++ src/test/java/Com/QA/Utilities/Utilities.java
@@ -1,14 +1,17 @@
 package Com.QA.Utilities;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.function.Function;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class Utilities {
@@ -36,30 +39,52 @@ public class Utilities {
 			Assert.assertTrue(false);
 		}
 	}
-	
-	public static void Click_With_Fluent(WebDriver driver,String locator) {
-		
+
+	public static void Click_With_Fluent(WebDriver driver,By locator) {
+
 		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
 				.withTimeout(Duration.ofSeconds(5))
 				.pollingEvery(Duration.ofSeconds(1))
 				.ignoring(NoSuchElementException.class);
-		
-		WebElement button = wait.until(
-				     new Function<WebDriver, WebElement>() {
 
-						
-						public WebElement apply(WebDriver t) {
-							
-							return driver.findElement(By.xpath(locator));
-						}
-					
-				     
-				     }
-				  );
-		
+		WebElement button = wait.until(
+
+				ExpectedConditions.elementToBeClickable(locator)
+				);
+
 		button.click();
 	}
-	
-	
-	
+
+
+	public static WebElement Find_Element_With_Fluent(WebDriver driver,By locator) {
+
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(5))
+				.pollingEvery(Duration.ofSeconds(1))
+				.ignoring(NoSuchElementException.class);
+
+		return wait.until(
+				new Function<WebDriver, WebElement>() {
+
+
+					public WebElement apply(WebDriver t) {
+
+						return driver.findElement(locator);
+					}
+
+
+				}
+				);
+
+	}
+
+	public static List<WebElement> Get_Multiple_Elements(WebDriver driver,By locator) {
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+		return wait.until(
+				ExpectedConditions.visibilityOfAllElementsLocatedBy(locator)
+				);
+	}
+
 }
